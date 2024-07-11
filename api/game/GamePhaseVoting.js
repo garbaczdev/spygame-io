f8bbadae454abc7f43033aae9cf4254c1c31ca5b
garbaczdev <maciej@garbacz.dev>
2024-07-11 20:08:52 +0200

    Voting phase backend implemented

--- api/game/GamePhaseVoting.js
+++ api/game/GamePhaseVoting.js
@@ -6,21 +6,36 @@ class GamePhaseVoting extends GamePhase {
   constructor(gameRoom, settings) {
     super(gameRoom);
     this.settings = settings;
+
     this.votes = {};
     this.gameRoom.players.forEach(player => {
-      this.votes[player] = null;
+      this.votes[player.name] = null;
     });
+
     this.votingFinished = false;
+    this.whoWon = "";
+    this.endVotesCount = {};
   }
 
   getPhaseState(player) {
+    
+    const endVotesCountToSend = {};
+    Object.keys(this.endVotesCount).forEach(playerName => {
+      const player = this.gameRoom.players.find(player => player.name === playerName);
+      endVotesCountToSend[player.name] = {
+        votes: this.endVotesCount[player.name],
+        isSpy: player.role.isSpy,
+      }
+    });
+
     return {
       name: "voting",
       state: {
         votingFinished: this.votingFinished,
-        voted: this.votes[player] !== null,
-        votedPlayerName: this.votes[player] !== null ? this.votes[player].name : "",
-        votes: {},
+        voted: this.votes[player.name] !== null,
+        votedPlayerName: this.votes[player.name] !== null ? this.votes[player.name].name : "",
+        endVotesCount: endVotesCountToSend,
+        whoWon: this.whoWon,
       }
     }
   }
@@ -29,16 +44,15 @@ class GamePhaseVoting extends GamePhase {
     if (actionData.phase === "voting" && actionData.type == "vote") {
       if (actionData.data.votedPlayerName === undefined) return;
       if (this.votingFinished) return;
-      if (this.votes[player] !== null) return;
+      if (this.votes[player.name] !== null) return;
 
       const votedPlayer = this.gameRoom.players.find(otherPlayer => otherPlayer.name === actionData.data.votedPlayerName);
       if (votedPlayer === undefined) return;
       if (votedPlayer.name === player.name) return;
 
-      this.votes[player] = votedPlayer;
-      console.log(this.votes);
+      this.votes[player.name] = votedPlayer;
 
-      if (Object.keys(this.votes).every(votedPlayer => votedPlayer !== null)){
+      if (Object.values(this.votes).every(votedPlayer => votedPlayer !== null)){
         this.finishVoting();
         this.gameRoom.updateAllPlayers();
       }
@@ -48,6 +62,7 @@ class GamePhaseVoting extends GamePhase {
 
     if (actionData.phase === "voting" && actionData.type == "finishVoting") {
       if (!player.isHost) return;
+      if (this.votingFinished) return;
       this.finishVoting();
       this.gameRoom.updateAllPlayers();
       return;
@@ -55,13 +70,37 @@ class GamePhaseVoting extends GamePhase {
 
     if (actionData.phase === "voting" && actionData.type == "restartGame") {
       if (!player.isHost) return;
+      if (!this.votingFinished) return;
+
       return;
     }
     super.action(player, actionData);
   }
 
   finishVoting() {
-    this.gameRoom.logger.info("Finished Voting");
+    if (this.votingFinished) return;
+
+    this.gameRoom.players.forEach(votingPlayer => {
+      this.endVotesCount[votingPlayer.name] = 0;
+    });
+
+    this.gameRoom.players.forEach(votingPlayer => {
+      if (this.votes[votingPlayer.name] === null) return;
+      this.endVotesCount[this.votes[votingPlayer.name].name] += 1;
+    });
+
+    const maxVotes = Math.max(...Object.values(this.endVotesCount));
+    if (
+      Object.keys(this.endVotesCount).filter(
+        playerName => this.endVotesCount[playerName] === maxVotes
+      ).every(playerName => {
+        const player = this.gameRoom.players.find(player => player.name === playerName);
+        return player.isSpy;
+      })
+    ) this.whoWon = "spies";
+    else this.whoWon = "roles";
+
+    this.gameRoom.logger.info(`Finished Voting with votes" ${JSON.stringify(this.endVotesCount)}. Winner: "${this.whoWon}"`);
     this.votingFinished = true;
   }
 }
