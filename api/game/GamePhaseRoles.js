eca3925a8c473d2cfe394b089d6236dfcd20381d
garbaczdev <maciej@garbacz.dev>
2024-07-11 11:54:37 +0200

    Roles distribution done!

--- api/game/GamePhaseRoles.js
+++ api/game/GamePhaseRoles.js
@@ -7,6 +7,7 @@ class GamePhaseRoles extends GamePhase {
   constructor(gameRoom, settings) {
     super(gameRoom);
     this.settings = settings;
+    this.distributeRoles();
   }
 
   getPhaseState(player) {
@@ -23,6 +24,19 @@ class GamePhaseRoles extends GamePhase {
       return;
     }
     super.action(player, actionData);
+  }
+
+  distributeRoles() {
+    const roleName = allRoles[Math.floor(Math.random() * allRoles.length)];
+
+    const shuffledPlayers = this.gameRoom.players.slice().sort(() => 0.5 - Math.random());
+
+    const spyPlayers = shuffledPlayers.slice(0, this.settings.spiesNumber);
+    const normalPlayers = shuffledPlayers.slice(this.settings.spiesNumber, shuffledPlayers.length);
+    
+    spyPlayers.forEach(player => player.setRole(new Role("", true)));
+    normalPlayers.forEach(player => player.setRole(new Role(roleName, false)));
+    this.gameRoom.logger.info(`ROLE: "${roleName}" SPIES: [${spyPlayers.map(player => player.name)}] NORMAL: [${normalPlayers.map(player => player.name)}]`);
   }
 }
 
