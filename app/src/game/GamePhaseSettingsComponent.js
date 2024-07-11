acf0bd554336d2cf493383ae0153c88d7c2ff39f
garbaczdev <maciej@garbacz.dev>
2024-07-11 10:51:54 +0200

    Fixed settings screen for non-host users

--- app/src/game/GamePhaseSettingsComponent.js
+++ app/src/game/GamePhaseSettingsComponent.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from 'react';
 
 export function GamePhaseSettingsComponent({socket, gameState}) {
 
-  const [spiesNumber, setSpiesNumber] = useState(gameState.phase.state.settings.spiesNumber);
-  const [discussionMinutes, setDiscussionMinutes] = useState(gameState.phase.state.settings.discussionMinutes);
+  const [spiesNumber, setSpiesNumber] = useState(gameState.phase.state?.settings?.spiesNumber ?? 0);
+  const [discussionMinutes, setDiscussionMinutes] = useState(gameState.phase.state?.settings?.discussionMinutes ?? 0);
 
   const canDecreaseSpies = spiesNumber > 1;
   const canDecreaseMinutes = discussionMinutes > 1;
@@ -14,60 +14,68 @@ export function GamePhaseSettingsComponent({socket, gameState}) {
 
   return (
     <>
-      <div className="container text-center">
-        <h2 className="mb-2">Settings</h2>
-        <h4 className="mb-5">Total players: {gameState.allPlayers.length}</h4>
-        <h4>Spies number</h4>
-        <div className="d-flex justify-content-center align-items-center mb-4 mt-2">
-          <button
-            className={`btn mx-2 ${canDecreaseSpies ? "btn-primary" : "btn-secondary"}`}
-            onClick={() => {
-              if (canDecreaseSpies) setSpiesNumber(spiesNumber - 1);
-            }}
-          >-</button>
-          <span className="mx-2">{spiesNumber}</span>
-          <button
-            className={`btn mx-2 ${canIncreaseSpies ? "btn-primary" : "btn-secondary"}`}
-            onClick={() => {
-              if (canIncreaseSpies) setSpiesNumber(spiesNumber + 1);
-            }}
-          >+</button>
-        </div>
-        <h4>Minutes for discussion</h4>
-        <div className="d-flex justify-content-center align-items-center mb-4 mt-2">
-          <button
-            className={`btn mx-2 ${canDecreaseMinutes ? "btn-primary" : "btn-secondary"}`}
-            onClick={() => {
-              if (canDecreaseMinutes) setDiscussionMinutes(discussionMinutes - 1);
-            }}
-          >-</button>
-          <span className="mx-2">{discussionMinutes}</span>
-          <button
-            className={`btn mx-2 ${canIncreaseMinutes ? "btn-primary" : "btn-secondary"}`}
-            onClick={() => {
-              if (canIncreaseMinutes) setDiscussionMinutes(discussionMinutes + 1);
-            }}
-          >+</button>
-        </div>
-        <button 
-          className="btn btn-lg btn-primary mt-4"
-          onClick={
-          () => {
-            socket.emit("action", {
-              phase: "settings",
-              type: "confirmSettings",
-              data: {
-                settings: {
-                  spiesNumber,
-                  discussionMinutes
+      {
+        gameState.player.isHost
+        ?
+        <div className="container text-center">
+          <h2 className="mb-2">Settings</h2>
+          <h4 className="mb-5">Total players: {gameState.allPlayers.length}</h4>
+          <h4>Spies number</h4>
+          <div className="d-flex justify-content-center align-items-center mb-4 mt-2">
+            <button
+              className={`btn mx-2 ${canDecreaseSpies ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => {
+                if (canDecreaseSpies) setSpiesNumber(spiesNumber - 1);
+              }}
+            >-</button>
+            <span className="mx-2">{spiesNumber}</span>
+            <button
+              className={`btn mx-2 ${canIncreaseSpies ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => {
+                if (canIncreaseSpies) setSpiesNumber(spiesNumber + 1);
+              }}
+            >+</button>
+          </div>
+          <h4>Minutes for discussion</h4>
+          <div className="d-flex justify-content-center align-items-center mb-4 mt-2">
+            <button
+              className={`btn mx-2 ${canDecreaseMinutes ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => {
+                if (canDecreaseMinutes) setDiscussionMinutes(discussionMinutes - 1);
+              }}
+            >-</button>
+            <span className="mx-2">{discussionMinutes}</span>
+            <button
+              className={`btn mx-2 ${canIncreaseMinutes ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => {
+                if (canIncreaseMinutes) setDiscussionMinutes(discussionMinutes + 1);
+              }}
+            >+</button>
+          </div>
+          <button 
+            className="btn btn-lg btn-primary mt-4"
+            onClick={
+            () => {
+              socket.emit("action", {
+                phase: "settings",
+                type: "confirmSettings",
+                data: {
+                  settings: {
+                    spiesNumber,
+                    discussionMinutes
+                  }
                 }
-              }
-            });
-          }
-        }>
-          Start Game
-        </button>
-      </div>
+              });
+            }
+          }>
+            Start Game
+          </button>
+        </div>
+        :
+        <div className="container text-center">
+          <h2>Waiting for host...</h2>
+        </div>
+      }
     </>
   );
 }
